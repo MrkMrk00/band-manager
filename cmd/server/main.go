71289a944444039be305a040e52665b73b30fcfa
Jiří Šuster <jiri.suster@o2.cz>
2024-02-14 19:03:00 +0100

    Buf generated service

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,37 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
+	"net/http"
 
-	"github.com/MrkMrk00/band-manager/pkg/database"
+	"connectrpc.com/connect"
+	rpcgen "github.com/MrkMrk00/band-manager/internal/rpcgen/rpc/v1"
+	"github.com/MrkMrk00/band-manager/internal/rpcgen/rpc/v1/rpcgenconnect"
 )
 
+type GreetServer struct{}
+
+func (s *GreetServer) Greet(
+	ctx context.Context,
+	req *connect.Request[rpcgen.GreetRequest],
+) (*connect.Response[rpcgen.GreetResponse], error) {
+	log.Println("Request headers: ", req.Header())
+	res := connect.NewResponse(&rpcgen.GreetResponse{
+		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
+	})
+	res.Header().Set("Greet-Version", "v1")
+	return res, nil
+}
+
 func main() {
-	fmt.Println("Hello, World!")
-
-	db := database.GetDB()
-
-	fmt.Println(db.MustExec("DROP TABLE \"TVOJE Máma\"").LastInsertId())
-
+	greeter := &GreetServer{}
+	mux := http.NewServeMux()
+	path, handler := rpcgenconnect.NewGreetServiceHandler(greeter)
+	mux.Handle(path, handler)
+	http.ListenAndServe(
+		"localhost:4000",
+		mux,
+	)
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,8 @@ module github.com/MrkMrk00/band-manager
 go 1.22.0
 
 require (
-	github.com/go-sql-driver/mysql v1.7.1 // indirect
-	github.com/jmoiron/sqlx v1.3.5 // indirect
+	connectrpc.com/connect v1.14.0
+	github.com/go-sql-driver/mysql v1.7.1
+	github.com/jmoiron/sqlx v1.3.5
+	google.golang.org/protobuf v1.32.0
 )
