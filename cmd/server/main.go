487ba8e921ff63de0e8969495f9378481f953f9e
Jiří Šuster <suster.ji@gmail.com>
2024-02-18 12:46:18 +0100

    gqlgen neni sranda xd

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -6,32 +6,25 @@ import (
 	"log"
 	"net/http"
 
-	"connectrpc.com/connect"
-	rpcgen "github.com/MrkMrk00/band-manager/internal/rpcgen/rpc/v1"
-	"github.com/MrkMrk00/band-manager/internal/rpcgen/rpc/v1/rpcgenconnect"
+	"github.com/99designs/gqlgen/graphql"
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/MrkMrk00/band-manager/internal/generated"
+	gql "github.com/MrkMrk00/band-manager/pkg/graphql"
 )
 
-type GreetServer struct{}
-
-func (s *GreetServer) Greet(
-	ctx context.Context,
-	req *connect.Request[rpcgen.GreetRequest],
-) (*connect.Response[rpcgen.GreetResponse], error) {
-	log.Println("Request headers: ", req.Header())
-	res := connect.NewResponse(&rpcgen.GreetResponse{
-		Greeting: fmt.Sprintf("Hello, %s!", req.Msg.Name),
-	})
-	res.Header().Set("Greet-Version", "v1")
-	return res, nil
-}
-
 func main() {
-	greeter := &GreetServer{}
-	mux := http.NewServeMux()
-	path, handler := rpcgenconnect.NewGreetServiceHandler(greeter)
-	mux.Handle(path, handler)
-	http.ListenAndServe(
-		"localhost:4000",
-		mux,
-	)
+	srv := handler.NewDefaultServer(generated.NewExecutableSchema(gql.NewResolver()))
+	srv.AroundFields(func(ctx context.Context, next graphql.Resolver) (res interface{}, err error) {
+		rc := graphql.GetFieldContext(ctx)
+		fmt.Println("Entered", rc.Object, rc.Field.Name)
+		res, err = next(ctx)
+		fmt.Println("Left", rc.Object, rc.Field.Name, "=>", res, err)
+		return res, err
+	})
+
+	http.Handle("/", playground.Handler("Starwars", "/query"))
+	http.Handle("/query", srv)
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
